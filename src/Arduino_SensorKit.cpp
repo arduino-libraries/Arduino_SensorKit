607c3c2dba032e322b64c619801c9378707f5b2a
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-11-03 16:39:15 +0100

    Fixed HW I2C for the display

--- src/Arduino_SensorKit.cpp
+++ src/Arduino_SensorKit.cpp
@@ -1,13 +1,13 @@
 #include "Arduino_Sensorkit.h"
 
 //Declare component's classes
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C Oled(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C Oled(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 DHT Environment(DHTPIN, DHTTYPE);
 SensorKit_LIS3DHTR Accelerometer;
 SensorKit_BMP280 Pressure;
 
 //Pointers to access them inside the class
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C& pOled = Oled;
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C& pOled = Oled;
 SensorKit_LIS3DHTR& pAccelerometer = Accelerometer;
 SensorKit_BMP280& pPressure = Pressure;
 DHT& pEnvironment = Environment;
