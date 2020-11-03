c58bb7d87d664e6ef153bb9fc0196d95f38e5c69
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-11-03 17:26:06 +0100

    Add files via upload

--- examples/Oled_Display/Oled_Display.ino
+++ examples/Oled_Display/Oled_Display.ino
@@ -2,17 +2,18 @@
 
 SensorKit kit;
 
-void setup(){
+void setup() {
   kit.begin();
 }
- 
-void loop(){
-  int random_value = random(0,1023);    //read value from A0
+
+void loop() {
+  int random_value = random(0, 1023);   //read value from A0
 
   Oled.clearBuffer();                   // clear the internal memory
   Oled.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
-  Oled.drawStr(0,10,"Analog Value:");    // write something to the internal memory
+  Oled.setCursor(0, 10);
+  Oled.print("Analog Value:");  // write something to the internal memory
   Oled.print(random_value);             // print random value read from pin A0
   Oled.sendBuffer();                    // transfer internal memory to the display
-  delay(1000);  
+  delay(1000);
 }
