c0ad352da7e5d634e0dcf05d7b6967a8da31ebb7
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-11-12 17:44:49 +0100

    Changed SensorKit_sketch example
    Now showing the temperature on screen

--- examples/SensorKit_sketch/SensorKit_sketch.ino
+++ examples/SensorKit_sketch/SensorKit_sketch.ino
@@ -2,40 +2,31 @@
 
 SensorKit kit;
 
-int pressure;
-int accelX;
-int humidity;
+float temperature;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  while (!Serial);
+  Serial.println("init");
+  
   kit.begin();
+  Oled.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  pressure = Pressure.readPressure();
-  accelX = Accelerometer.readX();
-  humidity = Environment.readHumidity();
-
-  Serial.print("Pressure: ");
-  Serial.println(pressure);
-  Serial.println();
-
-  Serial.print("Accel X: ");
-  Serial.println(accelX);
+  temperature = Pressure.readTemperature();
+  Serial.println(temperature);
   
-  Serial.print("Humidity: ");
-  Serial.println(humidity);
-
   printScreen();
-  delay(1000);
 }
 
 void printScreen() {
   Oled.clearBuffer();                   // clear the internal memory
-  Oled.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
-  Oled.drawStr(0, 10, "Analog Value:");  // write something to the internal memory
-  Oled.print(pressure);             // print random value read from pin A0
+  Oled.setCursor(0, 10);
+  Oled.print(temperature);             // print random value read from pin A0
+  Oled.print(" ºC");
   Oled.sendBuffer();                    // transfer internal memory to the display
+  delay(1000);
 }
