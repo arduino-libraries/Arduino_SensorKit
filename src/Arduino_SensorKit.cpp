177173b7de7aeba36bfd4c845cdc459e774d4ea4
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-12-10 11:57:28 +0100

    Add files via upload

--- src/Arduino_SensorKit.cpp
+++ src/Arduino_SensorKit.cpp
@@ -1,25 +1,7 @@
 #include "Arduino_Sensorkit.h"
 
 //Declare component's classes
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C Oled(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_HW_I2C Oled(U8X8_PIN_NONE);
 DHT Environment(DHTPIN, DHTTYPE);
 SensorKit_LIS3DHTR Accelerometer;
 SensorKit_BMP280 Pressure;
-
-SensorKit::SensorKit(){}
-
-bool SensorKit::begin(){
-  /*Oled.begin();
-  Oled.enableUTF8Print();
-
-  Accelerometer.begin();
-  delay(100);
-  Accelerometer.setOutputDataRate(LIS3DHTR_DATARATE_50HZ);
-
-  Pressure.begin();
-  Environment.begin();*/
-}
-
-void SensorKit::end(){
-
-}
