54fbb0d95b857a7325c40e16821ae1f0891ccf3e
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-12-12 11:38:36 +0100

    include fix

--- src/Arduino_SensorKit.cpp
+++ src/Arduino_SensorKit.cpp
@@ -1,4 +1,4 @@
-#include "Arduino_Sensorkit.h"
+#include "Arduino_SensorKit.h"
 
 //Declare component's classes
 U8X8_SSD1306_128X64_NONAME_SW_I2C Oled(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
