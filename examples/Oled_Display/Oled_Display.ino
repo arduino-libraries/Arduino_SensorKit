859bdf0eeb9ff90e112a7a8179e99177457f82b9
marqdevx <11246294+marqdevx@users.noreply.github.com>
2020-12-17 16:33:18 +0100

    Removed A0 comment
    Removed A0 from the comment , changed with the explanation of the line

--- examples/Oled_Display/Oled_Display.ino
+++ examples/Oled_Display/Oled_Display.ino
@@ -6,7 +6,7 @@ void setup() {
 }
 
 void loop() {
-  int random_value = random(0, 1023);   //read value from A0
+  int random_value = random(0, 1023);   // create a random value
 
   Oled.setFont(u8x8_font_chroma48medium8_r); 
   Oled.setCursor(0, 33);
