141929e5b556bb254fe2ee9cfff19e3cd0a477a8
Marc MERLIN <marc_soft@merlins.org>
2021-01-05 14:17:36 -0800

    fixes from noppatoppa

--- examples/Oled_Display/Oled_Display.ino
+++ examples/Oled_Display/Oled_Display.ino
@@ -9,11 +9,8 @@ void loop() {
   int random_value = random(0, 1023);   // create a random value
 
   Oled.setFont(u8x8_font_chroma48medium8_r); 
-  // Y needs to be a multiple of 9 for a font of 8 or the display
-  // will end up in an incorrect place.
-  Oled.setCursor(0, 27);
+  Oled.setCursor(0, 3);
   Oled.print("Value: ");
   Oled.print(random_value)
-  Oled.refreshDisplay();
   delay(1000);
 }
