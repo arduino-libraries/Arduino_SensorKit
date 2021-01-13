a135ba89cb21434a42eef8467eabc3746b90d95a
marqdevx <11246294+marqdevx@users.noreply.github.com>
2021-01-13 15:14:42 +0100

    Update Combined_Demo.ino

--- examples/Combined_Demo/Combined_Demo.ino
+++ examples/Combined_Demo/Combined_Demo.ino
@@ -1,72 +1,33 @@
 // Combined Demo by Marc MERLIN <marc_soft@merlins.org>
+// Reviewed by Pablo Marquínez  <p.marquinez@arduino.cc>
+// This demo use all the devices from the Arduino SensorKit
+// Showing the values on the Display
 
 #include "Arduino_SensorKit.h"
+
 #define BUZZER 5
+#define BUTTON  4
+#define LED 6
+#define POT A0
+#define MIC A2
+#define LIGHT A3
 
-uint8_t button = 4;
-uint8_t led = 6;
-uint8_t pot = A0;
-uint8_t mic = A2;
-uint8_t light = A3;
-
-// From https://playground.arduino.cc/Main/I2cScanner/
-// learn more about I2C here
-// https://www.seeedstudio.com/blog/2019/09/25/uart-vs-i2c-vs-spi-communication-protocols-and-uses/
-// Scanner from https://github.com/RobTillaart/Arduino/blob/master/sketches/MultiSpeedI2CScanner/MultiSpeedI2CScanner.ino
-// 30038	25	0x19		V	V	V	V	V	V	V	V
-// 30133	60	0x3C		V	V	V	V	V	V	V	V
-// 30296	119	0x77		V	V	V	V	V	V	V	V
-void i2c_scan() {
-  uint8_t error, address, nDevices;
- 
-  Wire.begin();
-  Serial.println("Scanning...");
- 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ )
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
- 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
- 
-      nDevices++;
-    }
-    else if (error==4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16)
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
-}
+int pot_value;
+bool button_state;
+int mic_value;
+int light_value;
 
 void setup() {
-  Serial.begin(115200);
-  // Running scan stops the OLED from working
-  i2c_scan();
+  Serial.begin(9600);
 
-  pinMode(mic , INPUT);
-  pinMode(light , INPUT);
-
-  pinMode(button , INPUT);
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
-
+  pinMode(MIC , INPUT);
+  pinMode(LIGHT , INPUT);
+  pinMode(BUTTON , INPUT);
+  
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  pinMode(BUZZER, OUTPUT);
+  
   Environment.begin();
 
   Oled.begin();
@@ -74,17 +35,14 @@ void setup() {
 
   Accelerometer.begin();
   Pressure.begin();
-
-  pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
-
   Oled.setFont(u8x8_font_amstrad_cpc_extended_r); 
 
-  // when using u8g8 instead of u8g2, cursor values
-  // are in characters, not pixels
+  //cursor values are in characters, not pixels
   Oled.setCursor(0, 4);
+  
   // If accelerometer and altimeter are queried too close to one another
   // this causes a hang, so we read this first.
   Oled.print("x:"); 
@@ -101,29 +59,31 @@ void loop() {
   Oled.setCursor(0, 0);
   Oled.print("But:"); 
 
-  uint16_t pot_value = analogRead(pot);
-  if (digitalRead(button)) {
-    digitalWrite(led, HIGH);
-    Oled.print("1"); 
+  pot_value = analogRead(POT);
+  
+  button_state = digitalRead(BUTTON);
+  Oled.print(button_state); 
+  
+  if (button_state == true) {
+    digitalWrite(LED, HIGH);
     tone(BUZZER, pot_value);
   } else {
-    digitalWrite(led, LOW);
-    Oled.print("0"); 
+    digitalWrite(LED, LOW);
     noTone(BUZZER);
   }
-
+  
   Oled.setCursor(0, 1);
   Oled.print("BuzPot: ");
   Oled.print(pot_value);
   Oled.print("Hz  ");
 
-  uint16_t mic_value = analogRead(mic);
+  mic_value = analogRead(MIC);
   Oled.setCursor(0, 2);
   Oled.print("Mic: ");
   Oled.print(mic_value);
   Oled.print("   ");
 
-  uint16_t light_value = analogRead(light);
+  light_value = analogRead(LIGHT);
   Oled.setCursor(0, 3);
   Oled.print("Light: ");
   Oled.print(light_value);
