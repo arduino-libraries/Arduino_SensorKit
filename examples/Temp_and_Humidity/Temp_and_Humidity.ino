61321e0ca011517ac66eee3d92faf085869d5cf6
Karl Söderby <35461661+karlsoderby@users.noreply.github.com>
2023-06-16 08:59:48 +0200

    Update DHT library
    Change the library from DHT sensor + adds a wrapper class specifically for I2C connection (DHT20)

--- examples/Temp_and_Humidity/Temp_and_Humidity.ino
+++ examples/Temp_and_Humidity/Temp_and_Humidity.ino
@@ -1,7 +1,12 @@
 //#define DHTPIN 3 // By default its connected to pin D3, it can be changed, define it before the #include of the library
 #include "Arduino_SensorKit.h"
 
+//uncomment line below if using DHT20
+//#define Environment Environment_I2C
+
 void setup() {
+  //uncomment line below if using DHT20
+  //Wire.begin();
   Serial.begin(9600);
   Environment.begin();
 }
--- src/Arduino_SensorKit.cpp
+++ src/Arduino_SensorKit.cpp
@@ -2,6 +2,7 @@
 
 //Declare component's classes
 U8X8_SSD1306_128X64_NONAME_SW_I2C Oled(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
-DHT Environment(DHTPIN, DHTTYPE);
+DHT Environment(DHTPIN,DHT11);
+DHT Environment_I2C(DHT20);
 SensorKit_LIS3DHTR Accelerometer;
 SensorKit_BMP280 Pressure;
