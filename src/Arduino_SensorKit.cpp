b2675e74493180aeee3039575470d7d0bc22d7d0
Alessandro Ranellucci <alessandro@pintle.it>
2023-11-07 19:17:44 +0100

    Fix compilation for GIGA R1

--- src/Arduino_SensorKit.cpp
+++ src/Arduino_SensorKit.cpp
@@ -1,7 +1,17 @@
 #include "Arduino_SensorKit.h"
 
+#if defined(ARDUINO_AVR_UNO)
+  #define _PIN_SDA SDA
+  #define _PIN_SCL SCL
+#elif defined(ARDUINO_GIGA)
+  #define _PIN_SDA I2C_SDA1
+  #define _PIN_SCL I2C_SCL1
+#else
+  #error "This board is not supported by Arduino_SensorKit"
+#endif
+
 //Declare component's classes
-U8X8_SSD1306_128X64_NONAME_SW_I2C Oled(/* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+U8X8_SSD1306_128X64_NONAME_SW_I2C Oled(/* clock=*/ _PIN_SCL, /* data=*/ _PIN_SDA, /* reset=*/ U8X8_PIN_NONE);
 DHT Environment(DHTPIN,DHT11);
 DHT Environment_I2C(DHT20);
 SensorKit_LIS3DHTR Accelerometer;
